6e0058808850407d2b1019c7b70c4075b908dac3
= <zain.fatima@dubizzlelabs.com>
2025-10-14 16:39:10 +0500

    work on arrays

--- index.php
+++ index.php
@@ -33,15 +33,55 @@
         "jujutsu kaisen", 
         "hunter x hunter"
     ];
+
+    $users = [
+        [
+            "name" => "Zain Fatima",
+            "Age" => 22,
+            "City" => "Karachi",
+            "Job" => "Software Engineer"
+        ],
+        [
+            "name" => "Wajeeha Rahman",
+            "Age" => 24,
+            "City" => "Islamabad",
+            "Job" => "Frontend Developer"
+        ],
+        [
+            "name" => "Zoraiz Ahsan",
+            "Age" => 26,
+            "City" => "Faisalabad",
+            "Job" => "Technical Consultant"
+        ],
+        [
+            "name" => "Maryam Sheikh",
+            "Age" => 23,
+            "City" => "Lahore",
+            "Job" => "Data Analyst"
+        ]
+    ]
     ?>
     <h1>My fav Animes</h1>
     <ul>
         <?php
-            foreach ($animes as $anime) {
-                echo "<li>{$anime}</li>";
-            }
+        foreach($animes as $items){
+            echo "<li>{$items}</li>";
+        }
         ?>
     </ul>
+    <p><?= $animes[2]?></p>
+    <br><br>
+    <h1>Users Information</h1>
+    <?php
+    foreach($users as $user){
+        echo "<h2>{$user['name']}</h2>";
+        echo "<p>Age: {$user['Age']}</p>";
+        echo "<p>City: {$user['City']}</p>";
+        echo "<p>Job: {$user['Job']}</p>";
+        echo "<hr>";
+    }
+    ?>
 
+    
 </body>
 </html>
